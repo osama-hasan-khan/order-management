b84ef96a48323c037bef07d238221fb0b212c527
osama-hasan-khan <osamahasankhan098@gmail.com>
2024-07-16 21:24:17 +0530

    connect mongodb

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import dataBaseConnection from "./config/db.js";
+import cookieParser from "cookie-parser";
 
 const PORT = process.env.PORT || 5000;
 
+
 const app = express();
 
-app.use(bodyParser.json());
+dotenv.config();
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
 app.use(cors());
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+dataBaseConnection();
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
